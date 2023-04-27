cff679a4279d7e1a34a8a99e27c8047e296c89fb
Dima Shutenko <58172685+dimashutenko@users.noreply.github.com>
2023-04-27 12:44:19 -0400

    26.04

--- venv/app.py
+++ venv/app.py
@@ -3,6 +3,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route('/')
 def main():
     return render_template("index.html")
+
+
+@app.route("/component1")
+def component_url():
+    return render_template("component_url.html")
+
+
+if __name__ == '__main__':
+    app.run()
